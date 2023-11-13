6f7ca5e45b08e6ec477880f66beb3271792b7275
huxiguo <hu@seekhoo.cn>
2023-11-13 20:35:53 +0800

    factory

---
+++ FactoryPattern.js
@@ -0,0 +1,59 @@
+// 工厂模式
+
+// 基类
+class Shape {
+  say() {
+    console.log(this.name);
+  }
+}
+
+// 长方形
+class Rectangle extends Shape {
+  constructor() {
+    super()
+    this.name = 'Rectangle'
+  }
+}
+
+// 正方形
+class Square extends Shape {
+  constructor() {
+    super()
+    this.name = 'Square'
+  }
+}
+
+// 圆形
+class Circle extends Shape {
+  constructor() {
+    super()
+    this.name = 'Circle'
+  }
+}
+
+// 工厂
+class ShapeFactory {
+  getShape(shapeType) {
+    switch (shapeType.toLowerCase()) {
+      case 'square':
+        return new Square()
+      case 'rectangle':
+        return new Rectangle()
+      case 'circle':
+        return new Circle()
+      default:
+        throw new Error('参数错误')
+    }
+  }
+}
+
+const shapeFactory = new ShapeFactory()
+
+const rectangle = shapeFactory.getShape('rectangle')
+rectangle.say()
+
+const square = shapeFactory.getShape('square')
+square.say()
+
+const circle = shapeFactory.getShape('circle')
+circle.say()
